aacbad00547ef07a0fe3b8c5b82aa0e187cd23f0
Stefan Kovarbasic <kovar.zmaj@gmail.com>
2020-04-20 20:59:31 +0200

    Final touch

--- JS/main.js
+++ JS/main.js
@@ -1,123 +1,113 @@
 
-var searchButton = document.querySelector(".search .button");
-var videoPreview = document.querySelector(".video-preview");
-var relatedVideos = document.querySelector(".related-videos");
-var loader = document.querySelector(".loader");
-var videoList = document.querySelector(".video-list");
-var pages = document.querySelector(".main-wrapper .pagination");
-var message = document.querySelector(".message");
+const searchButton = document.querySelector(".search .button");
+const videoPreview = document.querySelector(".video-preview");
+const relatedVideos = document.querySelector(".related-videos");
+const loader = document.querySelector(".loader");
+const videoList = document.querySelector(".video-list");
+const pages = document.querySelector(".main-wrapper .pagination");
+const message = document.querySelector(".message");
 
-var key = "AIzaSyAChgDP04ee0_gLIeMprFt2Ux6ARmQl0w8";
+const key = "AIzaSyCsVbQbUDtXdqCDSjGLRC5R04RcG0N5hz8";
 
-function loading() {
+const loading = ()=> {
 	loader.style.display = "block";
 	videoList.style.display = "none";
 	pages.style.display = "none";
 	videoPreview.innerHTML = '';
 	relatedVideos.innerHTML = '';
-	var rel = document.querySelector("h4");
+	let rel = document.querySelector("h4");
 	rel ? rel.textContent = "" : console.log ("no element");
 	message.style.display = "none";
 }
 
-function loaded() {
+const loaded = () => {
 	loader.style.display = "none";
 	videoList.style.display = "block";
 	pages.style.display = "block";
 }
 
-function onSearch() {
+const onSearch = () => {
 	loading();
 
-	var searchFiled = document.querySelector(".search input");
+	let searchFiled = document.querySelector(".search input");
 
 	searchFiled.value.trim() && getVideos(searchFiled.value);
 	searchFiled.value = '';
 }
 
-function getVideos(searchValue) {
-	var request = new XMLHttpRequest();
-
-	request.open("GET", 'https://www.googleapis.com/youtube/v3/search?part=snippet&\
-	type=video&maxResults=30&q=' + searchValue +'&key=' + key);
-
-	request.onload = function(){
-		listVideos(JSON.parse(request.responseText).items, "searched"); 
-		paginateVideos();
-	}
-
-	request.send();
+const getVideos = searchValue => {
+	let request = `https://www.googleapis.com/youtube/v3/search?part=snippet&
+	type=video&maxResults=30&q=${searchValue}&key=${key}`;
+	fetch(request)
+  	.then( response => response.json())
+  	.then( myJson => {
+  		listVideos(myJson.items, "searched");
+  		paginateVideos();
+  	})
+  	.catch( error => alert(`Error: ${error}`));
 }	
 
-function getRelatedVideos(someId) {
-	var request = new XMLHttpRequest();
+const getRelatedVideos = someId => {
+	let request = `https://www.googleapis.com/youtube/v3/search?part=snippet&
+	maxResults=6&type=video&relatedToVideoId=${someId}&key=${key}`;
+	fetch(request)
+  	.then( response => response.json())
+  	.then( myJson => listVideos(myJson.items, "related"))
+  	.catch( error => alert(`Error: ${error}`));
 
-	request.open("GET", 'https://www.googleapis.com/youtube/v3/search?part=snippet&\
-	maxResults=6&type=video&relatedToVideoId=' + someId +'&key=' + key);
-
-	request.onload = function(){
-		listVideos(JSON.parse(request.responseText).items, "related");
-	}
-
-	request.send();
 }
 
-function listVideos(videos, type) {
+const listVideos = (videos, type) => {
 	if (type === "searched") {
 		videoList.innerHTML = "";
 	} else {
 		relatedVideos.innerHTML = "";
-		var body = document.querySelector("body");
-		var heading = document.createElement("h4");
+		let body = document.querySelector("body");
+		let heading = document.createElement("h4");
 		heading.textContent = "Related videos:";
 		body.appendChild(heading);
 	}
 
-	videos.forEach(function(video) {
-		addVideo(video, type);
-	})	
+	videos.forEach( video => addVideo(video, type));
 }
 
-function addVideo(videoData, type) {
-	var videoElement = document.createElement("div");
-	var img = '<img src="' + videoData.snippet.thumbnails.medium.url +'">';
-	
+const addVideo = (videoData, type) => {
+	let videoElement = document.createElement("div");
+	let img = `<img src="${videoData.snippet.thumbnails.medium.url}">`;
+	let title, desc;
 	if (type === "searched") {
 		videoList.appendChild(videoElement);
-		var title = '<h3>' + videoData.snippet.title + '</h3>';
-		var desc ='<div>' + videoData.snippet.description + '</div>';
+		title = `<h3>${videoData.snippet.title}</h3>`;
+		desc =`<div>${videoData.snippet.description}</div>`;
 	} else {
 		relatedVideos.appendChild(videoElement);
-		var title = '<h3>' + videoData.snippet.title.substring(0,20) + "..." + '</h3>';
-		var desc ='<div>' + videoData.snippet.description.substring(0,60) + "..." + '</div>';
+		title = `<h3>${videoData.snippet.title.substring(0,20)}...</h3>`;
+		desc =`<div>${videoData.snippet.description.substring(0,60)}...</div>`;
 	}
 
-	videoElement.innerHTML = img + '<section>' + title + desc + '</section>';	
+	videoElement.innerHTML = `${img}<section>${title}${desc}</section>`;	
 
 	videoElement.querySelectorAll("h3, img").forEach(function(element) {
-		element.addEventListener("click", function() {
+		element.addEventListener("click", () => {
 			openVideo(videoData.id.videoId);
 			getRelatedVideos(videoData.id.videoId);
 		})
 	})
 }
 
-function openVideo(id) {
-	videoPreview.innerHTML = '<iframe width="800" height="450"\
-	 src="https://www.youtube.com/embed/' + id + '" frameborder="0"\
-	  allow="accelerometer; autoplay; encrypted-media; gyroscope; \
-	  picture-in-picture" allowfullscreen></iframe>';
-	  document.documentElement.scrollTop = 0;
+const openVideo = id => {
+	videoPreview.innerHTML =`<iframe width="800" height="450" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+	 document.documentElement.scrollTop = 0;
 }
 
-function paginateVideos() {
-	var mySect = [...videoList.childNodes];
+const paginateVideos = () => {
+	let mySect = [...videoList.childNodes];
 	videoList.innerHTML = "";
-	var pagination = document.querySelector(".main-wrapper .pagination");
+	let pagination = document.querySelector(".main-wrapper .pagination");
 	pagination.style.display = "block";
-	var mySpans = pagination.querySelectorAll("span");
+	let mySpans = pagination.querySelectorAll("span");
 
-	for (var i = 0 ; i <= 9; i++) {
+	for (let i = 0 ; i <= 9; i++) {
 		videoList.appendChild(mySect[i]);
 	}
 
@@ -128,11 +118,11 @@ function paginateVideos() {
 			videoList.innerHTML = "";
 			videoPreview.innerHTML = "";
 
-			for (var i = index*10 ; i <= index*10 + 9; i++) {
+			for (let i = index*10 ; i <= index*10 + 9; i++) {
 				videoList.appendChild(mySect[i]);
 			}
 
-			mySpans.forEach((span, num)=> {
+			mySpans.forEach((span, num) => {
 				if (num === index) {
 					span.classList.add("selected");
 				} else {
@@ -149,7 +139,7 @@ function paginateVideos() {
 
 searchButton.addEventListener("click", onSearch);
 
-var myInput = document.querySelector(".search input");
+const myInput = document.querySelector(".search input");
 myInput.addEventListener("keyup", e => {
 	if (e.key === "Enter") {
 		onSearch();
